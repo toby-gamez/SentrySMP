e732871f86c4f4b6f9323a54169eacb47db4e584
toby-gamez <henematobik213@gmail.com>
2025-10-12 17:09:28 +0200

    Update assembly informational version and sourcelink references across projects
    - Updated AssemblyInformationalVersion in SentrySMP.App, SentrySMP.Domain, and SentrySMP.Shared to new commit hash.
    - Updated sourcelink.json files to point to the new commit hash for accurate source linking.
    - Regenerated various cache and binary files in obj and bin directories for all projects.

--- SentrySMP.Domain/obj/Debug/net9.0/SentrySMP.Domain.AssemblyInfo.cs
+++ SentrySMP.Domain/obj/Debug/net9.0/SentrySMP.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SentrySMP.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e03a3f8714fc6be7b9fee4488bbc097f4b94081")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa96abd1440a1d9fabdc8f8e5b1e0e8fa8066698")]
 [assembly: System.Reflection.AssemblyProductAttribute("SentrySMP.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SentrySMP.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
