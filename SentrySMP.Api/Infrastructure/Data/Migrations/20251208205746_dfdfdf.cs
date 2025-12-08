a30bb6f0ca1f16e8d0d9abb3f37142a213503a99
toby-gamez <henematobik213@gmail.com>
2025-12-08 21:58:42 +0100

    Rename Shards table to Coins and update related foreign keys

---
+++ SentrySMP.Api/Infrastructure/Data/Migrations/20251208205746_dfdfdf.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SentrySMP.Api.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class dfdfdf : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shards_Servers_ServerId",
+                table: "Shards");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Shards",
+                table: "Shards");
+
+            migrationBuilder.RenameTable(
+                name: "Shards",
+                newName: "Coins");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Shards_ServerId",
+                table: "Coins",
+                newName: "IX_Coins_ServerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Coins",
+                table: "Coins",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Coins_Servers_ServerId",
+                table: "Coins",
+                column: "ServerId",
+                principalTable: "Servers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Coins_Servers_ServerId",
+                table: "Coins");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Coins",
+                table: "Coins");
+
+            migrationBuilder.RenameTable(
+                name: "Coins",
+                newName: "Shards");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Coins_ServerId",
+                table: "Shards",
+                newName: "IX_Shards_ServerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Shards",
+                table: "Shards",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shards_Servers_ServerId",
+                table: "Shards",
+                column: "ServerId",
+                principalTable: "Servers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
