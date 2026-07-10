1ff36c8097cf8dff8cadbf2a4c303a7b867d6a0f
toby-gamez <henematobik213@gmail.com>
2026-07-10 12:17:26 +0200

    Refactor Gem Service and Database Migrations
    - Removed IGemService interface as it is no longer needed.
    - Created migration to rename 'Gems' table to 'Coins' and updated related foreign keys.
    - Added new migration for the updated database schema reflecting the changes.

---
+++ SentrySMP.Api/Infrastructure/Data/Migrations/20260710095356_backInPast.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SentrySMP.Api.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class backInPast : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gems_Servers_ServerId",
+                table: "Gems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Gems",
+                table: "Gems");
+
+            migrationBuilder.RenameTable(
+                name: "Gems",
+                newName: "Coins");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Gems_ServerId",
+                table: "Coins",
+                newName: "IX_Coins_ServerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Coins",
+                table: "Coins",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Coins_Servers_ServerId",
+                table: "Coins",
+                column: "ServerId",
+                principalTable: "Servers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Coins_Servers_ServerId",
+                table: "Coins");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Coins",
+                table: "Coins");
+
+            migrationBuilder.RenameTable(
+                name: "Coins",
+                newName: "Gems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Coins_ServerId",
+                table: "Gems",
+                newName: "IX_Gems_ServerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Gems",
+                table: "Gems",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gems_Servers_ServerId",
+                table: "Gems",
+                column: "ServerId",
+                principalTable: "Servers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
